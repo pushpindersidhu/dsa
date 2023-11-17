22a6ef2372b1c83fbd34d26de2ee7faa7afe5b4c
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2023-11-17 06:40:47 -0800

    feat: test user posted code with jest

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,14 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 import DSA from "./data/dsa";
 import readFile from "./utils/readFile";
+import executeCode from "./utils/exec";
 
 const app: Express = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
@@ -29,8 +32,21 @@ app.get("/dsa", (req: Request, res: Response) => {
 });
 
 app.post("/test/:id", (req: Request, res: Response) => {
-  console.log(req.params.id);
-  res.send("Express + TypeScript Server");
+  const id = parseInt(req.params.id);
+  const code = req.body.code;
+
+  const dsa = DSA.find((dsa) => dsa.id === id);
+
+  if (!dsa) {
+    res.send("DSA not found");
+    return;
+  }
+
+  console.log(dsa.test);
+
+  const result = executeCode(code, dsa.test);
+
+  res.send(result);
 });
 
 app.listen(port, () => {
---
+++ server/interfaces/IDSA.ts
@@ -0,0 +1,9 @@
+export default interface IDSA {
+  id: number;
+  name: string;
+  description: string;
+  code: string;
+  solution: string;
+  completed: boolean;
+  test: string;
+}
