eea3ee5f053279e67524f179a0b756cbd213330e
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2023-11-18 18:01:11 -0800

    add code submission functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,26 +5,43 @@ import Sidebar from "./components/Sidebar";
 import { FaCode } from "react-icons/fa6";
 import Description from "./components/Description";
 
-import IDSA from "../../interfaces/IDSA";
+import IDSA from "../interfaces/IDSA";
+import axios from "axios";
 
 function App() {
   const [dsa, setDsa] = useState<IDSA[]>([]);
 
   const [selectedDsa, setSelectedDsa] = useState<IDSA>();
 
+  const [code, setCode] = useState<string>("");
+
   useEffect(() => {
-    fetch("http://localhost:3000/dsa")
-      .then((res) => res.json())
-      .then((data) => {
-        setDsa(data);
-        setSelectedDsa(data[0]);
-      });
+    axios.get("http://localhost:3000/dsa").then((res) => {
+      setDsa(res.data);
+      setSelectedDsa(res.data[0]);
+      setCode(res.data[0].code);
+    });
   }, []);
+
+  const handleSubmit = () => {
+    axios
+      .post(`http://localhost:3000/test/${selectedDsa?.id}`, {
+        ...selectedDsa,
+        code,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  };
 
   return (
     <div className="w-full h-screen grid grid-cols-12 bg-[#161618] gap-2 p-2">
-      <Navbar />
-      <Sidebar />
+      <Navbar onSubmit={handleSubmit} />
+      <Sidebar
+        dsa={dsa}
+        selectedDsa={selectedDsa}
+        setSelectedDsa={(dsa) => setSelectedDsa(dsa)}
+      />
       <div className="col-span-4 rounded-lg overflow-hidden bg-primary">
         <Description desc={selectedDsa?.description || ""} />
       </div>
@@ -37,7 +54,8 @@ function App() {
           height="100%"
           width="100%"
           defaultLanguage="typescript"
-          defaultValue={selectedDsa?.code || "//Write your code here"}
+          defaultValue={code}
+          onChange={(value) => setCode(value || "")}
           className="my-4 mx-2"
           theme="vs-dark"
           options={{ lineNumbers: "on", minimap: { enabled: false } }}
--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -5,60 +5,25 @@ import { FaLink } from "react-icons/fa6";
 import { TbBinaryTree } from "react-icons/tb";
 import { PiGraphFill } from "react-icons/pi";
 import { FaHashtag } from "react-icons/fa6";
+import IDSA from "../../interfaces/IDSA";
+import React from "react";
+import { IconType } from "react-icons";
 
-
-const Sidebar = () => {
-  const dataStructures = [
-    {
-      name: "Stack",
-      icon: RiStackFill,
-      completed: false,
-      code: "",
-      solution: "",
-    },
-    {
-      name: "Queue",
-      icon: HiMiniQueueList,
-      completed: false,
-      code: "",
-      solution: "",
-    },
-    {
-      name: "Linked List",
-      icon: FaLink,
-      completed: false,
-      code: "",
-      solution: "",
-    },
-    {
-      name: "Binary Tree",
-      icon: TbBinaryTree,
-      completed: false,
-      code: "",
-      solution: "",
-    },
-    {
-      name: "Binary Search Tree",
-      icon: PiTreeStructureFill,
-      completed: false,
-      code: "",
-      solution: "",
-    },
-    {
-      name: "Graph",
-      icon: PiGraphFill,
-      completed: false,
-      code: "",
-      solution: "",
-    },
-    {
-      name: "Hash Table",
-      icon: FaHashtag,
-      completed: false,
-      code: "",
-      solution: "",
-    },
-  ];
+const Sidebar: React.FC<{
+  dsa: IDSA[];
+  selectedDsa: IDSA | undefined;
+  setSelectedDsa: (dsa: IDSA) => void;
+}> = ({ dsa, selectedDsa, setSelectedDsa }) => {
+  const icons: { [key: string]: IconType } = {
+    Stack: RiStackFill,
+    Queue: HiMiniQueueList,
+    "Linked List": FaLink,
+    "Doubly Linked List": FaLink,
+    "Binary Tree": TbBinaryTree,
+    "Binary Search Tree": PiTreeStructureFill,
+    Graph: PiGraphFill,
+    "Hash Table": FaHashtag,
+  };
 
   return (
     <div className="bg-primary col-span-2 rounded-lg overflow-hidden">
@@ -68,15 +33,20 @@ const Sidebar = () => {
       </h3>
 
       <div className="flex flex-col gap-2 my-4 mx-2">
-        {dataStructures.map((dataStructure) => (
+        {dsa.map((d) => (
           <div
-            key={dataStructure.name}
-            className="flex items-center gap-2 px-2 py-3 rounded-lg hover:bg-secondary cursor-pointer"
+            key={d.id}
+            onClick={() => setSelectedDsa(d)}
+            className={
+              "flex items-center gap-2 px-2 py-3 rounded-lg hover:bg-secondary cursor-pointer" +
+              (selectedDsa?.id === d.id ? " bg-secondary" : "")
+            }
           >
-            <dataStructure.icon className="text-accent" />
-            <p className="text-gray-300 text-sm font-semibold px-1">
-              {dataStructure.name}
-            </p>
+            {React.createElement(icons[d.name], {
+              className: "text-accent",
+            })}
+
+            <p className="text-gray-300 text-sm font-semibold px-1">{d.name}</p>
           </div>
         ))}
       </div>
