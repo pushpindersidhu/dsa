9a41a33aea0b51529f6a2f83db569ccbdcd21a31
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2024-02-12 03:45:30 -0800

    Prim's MST

--- prim.py
+++ prim.py
@@ -14,7 +14,7 @@ class Prim:
         cost = 0
         visited = set()
         heap = [(0, 0)]
-        while heap or len(visited) != len(adj):
+        while heap and len(visited) != len(adj):
             w, u = heapq.heappop(heap)
             if u in visited:
                 continue
@@ -33,46 +33,44 @@ if __name__ == "__main__":
     import unittest
 
     class TestPrim(unittest.TestCase):
-        test_cases = [
-            {
-                "edges": [
-                    (0, 1, 1),
-                    (0, 2, 7),
-                    (1, 2, 5),
-                    (1, 3, 4),
-                    (2, 4, 6),
-                    (3, 4, 2),
-                    (1, 4, 3),
-                ],
-                "expected": 11,
-            },
-            {
-                "edges": [
-                    (0, 1, 4),
-                    (0, 2, 4),
-                    (1, 2, 2),
-                    (1, 0, 4),
-                    (2, 0, 4),
-                    (2, 1, 2),
-                    (2, 3, 3),
-                    (2, 5, 2),
-                    (2, 4, 4),
-                    (3, 2, 3),
-                    (3, 4, 3),
-                    (4, 2, 4),
-                    (4, 3, 3),
-                    (5, 2, 2),
-                    (5, 4, 3),
-                ],
-                "expected": 14,
-            },
-        ]
 
-        def test_mst_cost(self):
-            for i, test_case in enumerate(self.test_cases):
-                with self.subTest(i=i):
-                    edges = test_case["edges"]
-                    expected = test_case["expected"]
-                    self.assertEqual(Prim.mst_cost(edges), expected)
+        def test_prim(self):
+            test_cases = [
+                (
+                    [
+                        (0, 1, 1),
+                        (0, 2, 7),
+                        (1, 2, 5),
+                        (1, 3, 4),
+                        (2, 4, 6),
+                        (3, 4, 2),
+                        (1, 4, 3),
+                    ],
+                    11,
+                ),
+                (
+                    [
+                        (0, 1, 4),
+                        (0, 2, 4),
+                        (1, 2, 2),
+                        (1, 0, 4),
+                        (2, 0, 4),
+                        (2, 1, 2),
+                        (2, 3, 3),
+                        (2, 5, 2),
+                        (2, 4, 4),
+                        (3, 2, 3),
+                        (3, 4, 3),
+                        (4, 2, 4),
+                        (4, 3, 3),
+                        (5, 2, 2),
+                        (5, 4, 3),
+                    ],
+                    14,
+                ),
+            ]
+
+            for edges, expected in test_cases:
+                self.assertEqual(Prim.mst_cost(edges), expected)
 
     unittest.main()
